26a209cb0c2dad8bd6613c78989ea3286d1ebb17
Brian Andersen <b1@robocube.io>
2017-11-03 16:21:37 +0000

    Filter all but print statements here

--- RCL.Test/RunnerTest.cs
+++ RCL.Test/RunnerTest.cs
@@ -191,7 +191,7 @@ namespace RCL.Test
     {
       RCRunner runner = new RCRunner ();
       runner.Rep ("\"exit.o2\" save #pretty format {:exit 1}");
-      runner.Rep ("p:startx \"mono rcl.exe --output=clean --nokeys --program=exit.o2\"");
+      runner.Rep ("p:startx \"mono rcl.exe --output=clean --show=print --nokeys --program=exit.o2\"");
       Assert.AreEqual ("{status:1 data:\"<<Exec,exit status 1>>\"}", runner.Rep ("try {<-waitx $p}").ToString ());
     }
 
