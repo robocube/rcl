2cd2be3930fc9d88a31c29f43b8fa720e847285e
Brian Andersen <brian@robocube.io>
2018-07-09 20:08:48 -0600

    Add assert functions to call from csharp

--- RCL.Core/control/Throw.cs
+++ RCL.Core/control/Throw.cs
@@ -1,9 +1,11 @@
 
+using System;
+using System.Text;
 using RCL.Kernel;
 
 namespace RCL.Core
 {
-  public class Throw
+  public class Assert
   {
     [RCVerb ("assert")]
     public void EvalAssert (RCRunner runner, 
@@ -60,6 +62,50 @@ namespace RCL.Core
       runner.Finish (closure, 
                      new RCException (closure, RCErrors.Custom, right[0]), 
                      (int) RCErrors.Custom);
+    }
+
+    public static void Null (object obj)
+    {
+      if (obj != null)
+      {
+        throw new Exception (string.Format ("A null value was expected. Actual: {0}", obj.ToString ()));
+      }
+    }
+
+    public static void NotNull (object obj)
+    {
+      if (obj == null)
+      {
+        throw new Exception (string.Format ("A non-null value was expected."));
+      }
+    }
+
+    public static void AreEqual (string expected, string actual)
+    {
+      if (expected == null)
+      {
+        throw new Exception ("Expected string was null.");
+      }
+      else if (actual == null)
+      {
+        throw new Exception ("Actual string was null.");
+      }
+      else if (expected != actual)
+      {
+        StringBuilder builder = new StringBuilder ();
+        builder.AppendLine ("Strings are not equal.");
+        builder.AppendFormat ("Expected: {0}\n", expected);
+        builder.AppendFormat ("Actual:   {0}", actual);
+        throw new Exception (builder.ToString ());
+      }
+    }
+
+    public static void IsTrue (bool val)
+    {
+      if (!val)
+      {
+        throw new Exception ("Expected value was true.");
+      }
     }
   }
 }
