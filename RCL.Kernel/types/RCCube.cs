a341320f82fcd6faf9493aed8142acdee9ec91c5
Brian Andersen <brian@robocube.io>
2018-08-26 20:38:02 -0600

    Do not throw exceptions for case where timeline.Count == 0

--- RCL.Kernel/types/RCCube.cs
+++ RCL.Kernel/types/RCCube.cs
@@ -1255,9 +1255,6 @@ namespace RCL.Kernel
       if (timeline.Count == 0)
       {
         return 0;
-        //Console.WriteLine("timeline.Count == 0");
-        //Console.WriteLine(Environment.StackTrace);
-        throw new Exception ("VisitCellsForward may not be used on empty cubes");
       }
       //Row number in the source data grid.
       //NOT THE DESTINATION!
