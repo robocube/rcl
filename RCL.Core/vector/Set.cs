26594bcf30049ea5a67050b36c8280e159f6b5de
Brian Andersen <brian@robocube.io>
2019-10-10 14:35:45 -0600

    Make set honor the empty string as a name properly

--- RCL.Core/vector/Set.cs
+++ RCL.Core/vector/Set.cs
@@ -13,18 +13,19 @@ namespace RCL.Core
   public class Set
   {
     [RCVerb ("set")]
-    public virtual void EvalSet (
-      RCRunner runner, RCClosure closure, RCBlock left, RCBlock right)
+    public virtual void EvalSet (RCRunner runner, RCClosure closure, RCBlock left, RCBlock right)
     {
       //We are going to need a faster version of this at some point.
       RCBlock result = RCBlock.Empty;
       HashSet<string> handled = new HashSet<string> ();
+      bool leftHasNames = left.HasNamedVariables ();
       for (int i = 0; i < left.Count; ++i)
       {
         RCBlock lname = left.GetName (i);
         RCBlock rname;
-        if (lname.Name == "")
+        if (!leftHasNames)
         {
+          // In this case left and right must have equal count or else BOOM.
           rname = right.GetName (i);
         }
         else
@@ -34,14 +35,12 @@ namespace RCL.Core
         //rname.Value is null in the case of the empty block
         if (rname != null && rname.Value != null)
         {
-          result = new RCBlock (
-            result, rname.Name, rname.Evaluator, rname.Value);
+          result = new RCBlock (result, rname.Name, rname.Evaluator, rname.Value);
           handled.Add (rname.Name);
         }
         else
         {
-          result = new RCBlock (
-            result, lname.Name, lname.Evaluator, lname.Value);
+          result = new RCBlock (result, lname.Name, lname.Evaluator, lname.Value);
         }
       }
       for (int i = 0; i < right.Count; ++i)
@@ -49,8 +48,7 @@ namespace RCL.Core
         RCBlock rname = right.GetName (i);
         if (!handled.Contains (rname.Name))
         {
-          result = new RCBlock (
-            result, rname.Name, rname.Evaluator, rname.Value);
+          result = new RCBlock (result, rname.Name, rname.Evaluator, rname.Value);
         }
       }
       runner.Yield (closure, result);
