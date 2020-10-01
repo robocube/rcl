86a44656205dce0bb96e91729e4152e540400f2c
Brian Andersen <brian@robocube.io>
2020-10-01 10:29:48 -0600

    Build/test scripts for windows (.net) and wsl (mono)

---
+++ build.bat
@@ -0,0 +1,15 @@
+
+msbuild
+RCL.Exe\bin\Debug\rcl.exe --program=RCL.Test\rclkit\rcl\rclt.rcl^
+ RCL.Test\rclkit\tests\CoreTest1.rclt^
+ RCL.Test\rclkit\tests\CoreTest2.rclt^
+ RCL.Test\rclkit\tests\CoreTest3.rclt^
+ RCL.Test\rclkit\tests\EvalTest.rclt^
+ RCL.Test\rclkit\tests\CubeTest.rclt^
+ RCL.Test\rclkit\tests\LexerTest.rclt^
+ RCL.Test\rclkit\tests\BlockTest.rclt^
+ RCL.Test\rclkit\tests\RunnerTest.rclt^
+ RCL.Test\rclkit\tests\ParserTest.rclt^
+ RCL.Test\rclkit\tests\rcl.log_parse.rclt^
+ RCL.Test\rclkit\tests\rcl.md_parse.rclt^
+ --action=main --rclt-display-status=F,K,P
